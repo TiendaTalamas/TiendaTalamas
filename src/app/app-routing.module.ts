44a7832d46ccb852654338aeaffc2f5554defc47
Brandon Moreno <morenob2525@gmail.com>
2020-02-05 18:29:04 -0600

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PrimerComponent} from './componentes/primer/primer.component'
 import {InicioComponent} from './componentes/componentes/inicio/inicio.component'
-import {PieComponent} from './componentes/footer/pie/pie.component'
+
 
 
 const routes: Routes = [
   {path: '', component: InicioComponent, }, 
     {path: 'primer', component: PrimerComponent, },
-    {path: 'pie', component: PieComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimerComponent } from './componentes/primer/primer.component';
 import { InicioComponent } from './componentes/componentes/inicio/inicio.component';
-import { PieComponent } from './componentes/footer/pie/pie.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimerComponent,
     InicioComponent,
-    PieComponent
+   
   ],
   imports: [
     BrowserModule,
