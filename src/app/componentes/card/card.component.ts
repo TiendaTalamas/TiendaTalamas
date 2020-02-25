5ebc87f90a82754fe5045d75d7db87bca13bd6f6
Oscar MS <oscar-ms8@hotmail.com>
2020-02-24 18:19:24 -0600

    Cambios Login

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { Response } from '@angular/http';
+import { URLSearchParams } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+import {Location} from "@angular/common";
+import { producto } from '../../servicios/producto';
+import { servicioCompartido } from '../../servicios/servicioCompartido';
+
 
 @Component({
   selector: 'app-card',
@@ -6,10 +17,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  DatosError:boolean = false;
+ 
+  registroForm: FormGroup;
+  email: string;
+  contrasena: string;
+  AA: string;
+  data: any[];
+  val: any[];
+  contenedor: string;
+  xxxMap = new Map();
+  valuesKeys = new Array;
+  articulosArray = new Array;
+ngOnInit(){
 
-  constructor() { }
+}
 
-  ngOnInit() {
+  constructor(private router: Router, private location:Location,private fb: FormBuilder,private http: Http){
+    this.registroForm = fb.group({
+      'email' : [null, Validators.required],
+  
+      'contrasena': this.contrasena,
+
+    });}
+
+  registrar() {     
+    if(!this.DatosError)
+    {
+      
+    let body = new URLSearchParams();
+  
+    body.append('email', this.email);
+  
+    body.append('contrasena', this.contrasena);
+
+  
+    console.log(this.email);
+   
+    console.log(this.contrasena);
+
+    this.http.post('http://192.168.1.99/talamas/inicioSesion.php', body)
+    .map((res:Response) => res.json())
+            .subscribe(result => 
+              {
+              this.AA = "";
+            this.data = [];
+            console.log(result);
+            this.articulosArray = result;
+            for (var key in result) {
+            this.AA = this.AA + key;
+            if (result.hasOwnProperty(key)) {
+              this.val = result[key];
+              this.data.push(Object.keys(this.val));
+              for (var i = 0; i < Object.keys(this.val).length; i++) {
+              this.contenedor = Object.keys(this.val)[i];
+              Object.entries(this.val)[i]
+               
+                this.xxxMap.set(Object.keys(this.val)[i], Object.values(this.val)[i]);
+                this.valuesKeys.push(Object.keys(this.val)[i], Object.values(this.val)[i]);
+
+                }
+             }
+          }
+    });
+
+    }
+
+    
   }
+
 
 }
