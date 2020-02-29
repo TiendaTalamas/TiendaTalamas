969778fd65c31a4cb8da5a05f6a4300c8ae4fc58
Oscar MS <oscar-ms8@hotmail.com>
2020-02-29 13:41:40 -0600

    Se agrego la card en el componente categorias

--- src/app/componentes/categoria-producto/categoria-producto.component.ts
+++ src/app/componentes/categoria-producto/categoria-producto.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import { servicioCompartido } from 'src/app/servicios/servicioCompartido';
+import 'rxjs/add/operator/map';
+import { URLSearchParams } from "@angular/http";
+import {Location} from "@angular/common";
+import { producto } from '../../servicios/producto';
+
+
 
 @Component({
   selector: 'app-categoria-producto',
@@ -7,9 +16,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaProductoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http,private router: Router, private location:Location,
+    private _servicioCompartido : servicioCompartido) { }
 
   ngOnInit() {
+  }
+  
+  navegarInicio()
+  {
+    this.router.navigate(['']);
+    
+  }
+  
+  navegarSesion()
+  {
+
+    this.router.navigate(['card'])
+
+
+  }
+  navegarRegistro()
+  {
+    this.router.navigate(['registro']);
+    
+  }
+  navegarLibreria()
+  {
+    this.router.navigate(['libreria'])
+  }
+
+  navegarPublicidad()
+  {
+    this.router.navigate(['emd'])
+  }
+  navegarMusica()
+  {
+    this.router.navigate(['musica'])
   }
 
 }
