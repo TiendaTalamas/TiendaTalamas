665dd5cfeb192d38e281f9e7fa13c001c6906817
Brandon Moreno <morenob2525@gmail.com>
2020-03-05 14:14:34 -0600

    123

--- src/app/componentes/venta-libro/venta-libro.component.ts
+++ src/app/componentes/venta-libro/venta-libro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 //Importando los servicios para obtener los datos del producto
 import {servicioCompartido} from '../../servicios/servicioCompartido';
 import { producto } from '../../servicios/producto';
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-venta-libro',
   templateUrl: './venta-libro.component.html',
@@ -10,10 +11,43 @@ import { producto } from '../../servicios/producto';
 export class VentaLibroComponent implements OnInit {
   //array para guardar los valores
   productos: any[];
-  constructor(private _servicioCompartido : servicioCompartido) { }
+  constructor(private _servicioCompartido : servicioCompartido,private router:Router) { }
+  
 
   ngOnInit() {
     this.productos = this._servicioCompartido.getProductoData();  
+  }
+
+  navegarInicio()
+  {
+    this.router.navigate(['']);
+    
+  }
+  
+  navegarSesion()
+  {
+
+    this.router.navigate(['card'])
+
+
+  }
+  navegarRegistro()
+  {
+    this.router.navigate(['registro']);
+    
+  }
+  navegarLibreria()
+  {
+    this.router.navigate(['libreria'])
+  }
+
+  navegarPublicidad()
+  {
+    this.router.navigate(['emd'])
+  }
+  navegarMusica()
+  {
+    this.router.navigate(['musica'])
   }
 
 }
