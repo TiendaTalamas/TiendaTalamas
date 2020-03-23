7ddf740fb13379417011c1c8507f567f64f3135d
Jhzeg <jahaziel@emdpublicidad.com>
2020-03-23 14:16:41 -0600

    ComponenteConfiguracion

--- src/app/componentes/configuracion-usuario/configuracion-usuario.component.ts
+++ src/app/componentes/configuracion-usuario/configuracion-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import { servicioCompartido } from '../../servicios/servicioCompartido';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-configuracion-usuario',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuracion-usuario.component.css']
 })
 export class ConfiguracionUsuarioComponent implements OnInit {
+  cadena: string;
+  registroForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router, private _servicioCompartido : servicioCompartido, private fb: FormBuilder) { 
+    this.registroForm = fb.group({
+      'cadena' : this.cadena
+    
+  });}
 
   ngOnInit() {
   }
+
+  navegarInicio()
+  {
+    this.router.navigate(['']);
+    
+  }
+
+  navegarSesion()
+  {
+
+    this.router.navigate(['card'])
+
+
+  }
+  navegarRegistro()
+  {
+    this.router.navigate(['registro']);
+    
+  }
+  navegarLibreria()
+  {
+    this.router.navigate(['libreria'])
+  }
+
+  navegarPublicidad()
+  {
+    this.router.navigate(['emd'])
+  }
+  navegarMusica()
+  {
+    this.router.navigate(['musica']);
+
+  }
+
+  navegarBusqueda()
+  {
+    this._servicioCompartido.setCadena(this.cadena);
+    console.log(this.cadena);
+    this.router.navigate(['busqueda'])
+  }
+
 
 }
