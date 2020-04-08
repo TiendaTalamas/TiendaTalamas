3c657abc9a49741100d8ec3b3baddaf3ae999903
Oscar MS <oscar-ms8@hotmail.com>
2020-04-08 16:26:38 -0500

    Cambiar contrasena

--- src/app/componentes/configuracion-usuario/configuracion-usuario.component.ts
+++ src/app/componentes/configuracion-usuario/configuracion-usuario.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import { servicioCompartido } from '../../servicios/servicioCompartido';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import { URLSearchParams } from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-configuracion-usuario',
@@ -11,12 +15,21 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ConfiguracionUsuarioComponent implements OnInit {
   cadena: string;
   registroForm: FormGroup;
-
-  constructor(private router: Router, private _servicioCompartido : servicioCompartido, private fb: FormBuilder) { 
+  cambioContrasena: FormGroup;
+  contrasenaActual: string;
+  contrasenaNueva:string;
+  contrasenaRepetida:string;
+  constructor(private router: Router, private _servicioCompartido : servicioCompartido, private fb: FormBuilder, private fb2: FormBuilder, private http: Http) { 
     this.registroForm = fb.group({
       'cadena' : this.cadena
     
-  });}
+  })
+  this.cambioContrasena = fb2.group({
+    'contrasenaActual' : this.contrasenaActual,
+    'nuevaContrasena'  : this.contrasenaNueva,
+    'contrasenaRepetida' : this.contrasenaRepetida
+  })
+  ;}
 
   ngOnInit() {
   }
@@ -61,5 +74,32 @@ export class ConfiguracionUsuarioComponent implements OnInit {
     this.router.navigate(['busqueda'])
   }
 
+  cambiarContrasena()
+  {
+    let body = new URLSearchParams();
+  
+    body.append('email', localStorage.getItem('email_U'));
+    body.append('contrasenaActual', this.contrasenaActual);
+    body.append('contrasenaNueva', this.contrasenaNueva);
+    body.append('contrasenaRepetida', this.contrasenaRepetida);
+  
+    console.log(localStorage.getItem('email_U'));
+   
+    this.http.post('http://192.168.1.99/talamas/CambiarContrasena.php', body)
+    .map((res:Response) => res.text())
+            .subscribe(result => 
+              {
+                if(result == "OK")
+                {
+                  alert("Cambio realizado correctamente");
+                }
+                else if(result == "ERROR")
+                {
+                  alert("Contaseña no valida")
+                }
+    });
+
+    
+  }
 
 }
