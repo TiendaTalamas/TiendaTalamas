2895795918b8e2f7ebc968763043a082edf9bf3a
Oscar MS <oscar-ms8@hotmail.com>
2020-06-09 14:47:52 -0500

    Bñañba

--- src/app/componentes/metodo-pago/metodo-pago.component.ts
+++ src/app/componentes/metodo-pago/metodo-pago.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import { servicioCompartido } from '../../servicios/servicioCompartido';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import { URLSearchParams } from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-metodo-pago',
@@ -6,10 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./metodo-pago.component.css']
 })
 export class MetodoPagoComponent implements OnInit {
-
-  constructor() { }
+  AA: string;
+  data: any[];
+  val: any[];
+  contenedor: string;
+  xxxMap = new Map();
+  valuesKeys = new Array;
+  direccionesArray = new Array;
+  constructor(private router: Router, private _servicioCompartido : servicioCompartido, private fb: FormBuilder, private fb2: FormBuilder,private fb3: FormBuilder, private http: Http) { }
 
   ngOnInit() {
+    this.obtenerDirecciones();
   }
+  obtenerDirecciones() {
+    let body = new URLSearchParams();
+    body.append('email', localStorage.getItem('email_U'));
+    this.http.post('http://192.168.1.99/talamas/obtenerDirecciones.php', body)
+    .map((res:Response) => res.json())
+            .subscribe(result => 
+            {
+            this.AA = "";
+            this.data = [];
+            console.log(result);
+            this.direccionesArray = result;
+            for (var key in result) {
+            this.AA = this.AA + key;
+            if (result.hasOwnProperty(key)) {
+              this.val = result[key];
+              this.data.push(Object.keys(this.val));
+              for (var i = 0; i < Object.keys(this.val).length; i++) {
+              this.contenedor = Object.keys(this.val)[i];
+              Object.entries(this.val)[i]
+               
+                this.xxxMap.set(Object.keys(this.val)[i], Object.values(this.val)[i]);
+                this.valuesKeys.push(Object.keys(this.val)[i], Object.values(this.val)[i]);
 
+                }
+             }
+          }
+    });
+  }
 }
