71e4984f3a81c2fbcac3477db7350c73cf455c46
Oscar MS <oscar-ms8@hotmail.com>
2020-07-16 15:12:06 -0500

    Url Parametrizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {path: 'card', component: CardComponent, },
   {path: 'registro', component: RegistroComponent, },
   {path: 'libreria', component: LibreriaComponent, },
-  {path: 'venta', component: VentaLibroComponent, },
+  {path: 'venta/:categoria/:id', component: VentaLibroComponent, },
   {path: 'emd', component: EMDComponent, },
   {path: 'musica', component: MusicaComponent,},
   {path: 'categoria', component: CategoriaProductoComponent,}, 
