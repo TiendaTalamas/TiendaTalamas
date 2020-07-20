740254de7a199f6f76efe4ca0165e60732303111
Oscar MS <oscar-ms8@hotmail.com>
2020-07-20 14:41:43 -0500

    Hice el trabajo de brandon

--- src/app/componentes/compra/compra/compra.component.ts
+++ src/app/componentes/compra/compra/compra.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder,Validators } from '@angular/forms';
 import {Router, ActivatedRoute} from "@angular/router";
 import { Http , Response} from '@angular/http';
 import { URLSearchParams } from "@angular/http";
@@ -13,8 +12,7 @@ import {Location} from "@angular/common";
 })
 export class CompraComponent implements OnInit {
 
-  ventaForm: FormGroup;
-  ventaforma: FormGroup;
+
   email: string;
   nombre: string;
   apellido: string;
@@ -25,42 +23,25 @@ export class CompraComponent implements OnInit {
   NumeroExterior:string;
   CodigoPostal:string;
   numeroCelular:string;
-  constructor(private router:Router,private http:Http,private fb: FormBuilder,fb2: FormBuilder,private Route:ActivatedRoute) {
-    this.ventaForm = fb.group({
-      'email' : [null, Validators.required],
-      'nombre': this.nombre,
-      'apellido': this.apellido,  
-      'calle1': this.calle1,
-      'calle2': this.calle2,
-      'calle3': this.calle3,
-      'ciudad': this.ciudad,
-      'NumeroExterior': this.NumeroExterior,
-      'CodigoPostal': this.CodigoPostal,
-      'numeroCelular': this.numeroCelular
-    });
+  constructor(private router:Router,private http:Http,private Route:ActivatedRoute) {
+ 
    }
 IdProducto:string;
   ngOnInit() {
     this.IdProducto = this.Route.snapshot.paramMap.get('id');
   }
   onSubmit() {
-    // TODO: Use EventEmitter with form value
-  }
-
-  continuarDatosBancarios()
-  {
     let body = new URLSearchParams();
     body.append("Nombre", this.nombre);
     body.append("Apellido",this.apellido);
     body.append("NumeroTelefonico", this.numeroCelular);
     body.append("email", this.email)
     this.http.post('http://emdpublicidad.com/tiendatalamas/archivos/php/pagoRedirigido.php', body)
-    .map((res:Response) => res.json())
-            .subscribe(result => 
-            {
-
-    });
   }
 
+  continuarDatosBancarios()
+  {
 
+
+  }
 }
