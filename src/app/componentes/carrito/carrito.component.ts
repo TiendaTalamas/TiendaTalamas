fa70719428a76d57e6659391919f07a700dc6d99
RecGame <morenob2525@gmail.com>
2020-07-22 17:43:43 -0500

    Cambios carrito

--- src/app/componentes/carrito/carrito.component.ts
+++ src/app/componentes/carrito/carrito.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router,} from "@angular/router";
+import { Http,} from '@angular/http';
+
 
 @Component({
   selector: 'app-carrito',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http,private router: Router) {}
 
   ngOnInit() {
+  }
+  navegarInicio()
+  {
+    this.router.navigate(['']);
+    
+  }
+
+  navegarSesion()
+  {
+
+    this.router.navigate(['card'])
+
+
+  }
+  navegarRegistro()
+  {
+    this.router.navigate(['registro']);
+    
+  }
+  navegarLibreria()
+  {
+    this.router.navigate(['libreria'])
+  }
+
+  navegarPublicidad()
+  {
+    this.router.navigate(['emd'])
+  }
+  navegarMusica()
+  {
+    this.router.navigate(['musica']);
+
+  }
+
+  navegarConfiguracion()
+  {
+    this.router.navigate(['ConfiguracionUsuario']);
   }
 
 }
