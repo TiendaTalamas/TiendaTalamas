4f1d3d888fd1bd4c2ac049dec7a743eb208c5087
Oscar MS <oscar-ms8@hotmail.com>
2020-07-30 18:12:00 -0500

    cambio

--- src/app/componentes/historial/historial/historial.component.ts
+++ src/app/componentes/historial/historial/historial.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute} from "@angular/router";
-import { Http , Response} from '@angular/http';
-
+import { Http, Response } from '@angular/http';
+import { URLSearchParams } from "@angular/http";
+import 'rxjs/add/operator/map';
+import {Router} from "@angular/router";
+import {Location} from "@angular/common";
+import { servicioCompartido } from '../../../servicios/servicioCompartido';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-historial',
@@ -11,14 +15,57 @@ import { Http , Response} from '@angular/http';
 export class HistorialComponent implements OnInit {
 
   cadena: string;
-  constructor(private router:Router,private http:Http,) { }
+  constructor(private router:Router,private http:Http,private _servicioCompartido:servicioCompartido) { }
 
   ngOnInit() {
   }
+
+  registroForm:FormGroup;
+  AA: string;
+  data: any[];
+  val: any[];
+  contenedor: string;
+  xxxMap = new Map();
+  valuesKeys = new Array;
+  articulosArray = new Array;
+  Subtotal:string;
+
   navegarInicio()
   {
     this.router.navigate(['']);
-    
+    this.obtenerCompras();
+  }
+
+  obtenerCompras()
+  {
+    let body = new URLSearchParams();
+    body.append("token",localStorage.getItem('Token'));
+    this.http.post('http://emdpublicidad.com/tiendatalamas/archivos/php/carrito.php', body)
+    .map((res:Response) => res.json())
+            .subscribe(result => 
+            {
+            this.AA = "";
+            this.data = [];
+            console.log(result);
+            this.articulosArray = result;
+            console.log(this.articulosArray);
+            for (var key in result) {
+            this.AA = this.AA + key;
+            if (result.hasOwnProperty(key)) {
+              this.val = result[key];
+              this.data.push(Object.keys(this.val));
+              for (var i = 0; i < Object.keys(this.val).length; i++) {
+              this.contenedor = Object.keys(this.val)[i];
+              Object.entries(this.val)[i]
+               
+                this.xxxMap.set(Object.keys(this.val)[i], Object.values(this.val)[i]);
+                this.valuesKeys.push(Object.keys(this.val)[i], Object.values(this.val)[i]);
+
+                }
+             }
+          }
+    });
+
   }
   
   navegarSesion()
