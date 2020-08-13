31765bf7689ebc7a69d9b5e114606551bb8cf68a
Oscar MS <oscar-ms8@hotmail.com>
2020-08-13 13:55:08 -0500

    Quite el navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { URLSearchParams } from "@angular/http";
 import 'rxjs/add/operator/map';
 import {Router} from "@angular/router";
 import {Location} from "@angular/common";
+import { servicioCompartido } from './servicios/servicioCompartido';
   
 
 
@@ -23,7 +24,7 @@ export class AppComponent {
   cadena: string;
 
 
-  constructor(private router: Router, private location:Location,private http: Http)
+  constructor(private router: Router, private location:Location,private http: Http, public _servicioCompartido:servicioCompartido)
 
   
   
@@ -31,10 +32,135 @@ export class AppComponent {
   ngOnInit()
   {
 
+    this._servicioCompartido.comprobarUsuario();
+    this.obtenerSubCategoriasLibros();
+    this.obtenerSubCategoriasInst();
+  }
+  AA_Sub: string;
+  data_Sub: any[];
+  val_Sub: any[];
+  contenedor_Sub: string;
+  xxxMap_Sub = new Map();
+  valuesKeys_Sub = new Array;
+  articulosArray_Sub = new Array;
+  articulosArray_Inst = new Array;
+
+  navegarCategoria(Categoria:string, SubCategoria: string){
+
+    console.log(SubCategoria);
+    this.router.navigate(['categoria',Categoria,SubCategoria]);
+
+  this._servicioCompartido.setCategoria(Categoria);
+  this._servicioCompartido.setSubCategoria(SubCategoria);
 
   
   }
-  
+  obtenerSubCategoriasLibros(){
+    
+    
+    let body2 = new URLSearchParams();
+    body2.append('categoria', "Libros");
+
+
+
+    this.http.post('http://emdpublicidad.com/tiendatalamas/archivos/php/obtenerSubCategoria.php', body2)
+    .map((res:Response) => res.json())
+            .subscribe(result => 
+              {
+              this.AA_Sub = "";
+            this.data_Sub = [];
+            console.log(result);
+            this.articulosArray_Sub = result;
+            for (var key in result) {
+            this.AA_Sub = this.AA_Sub + key;
+            if (result.hasOwnProperty(key)) {
+              this.val_Sub = result[key];
+              this.data_Sub.push(Object.keys(this.val_Sub));
+              for (var i = 0; i < Object.keys(this.val_Sub).length; i++) {
+              this.contenedor_Sub = Object.keys(this.val_Sub)[i];
+              Object.entries(this.val_Sub)[i]
+               
+                this.xxxMap_Sub.set(Object.keys(this.val_Sub)[i], Object.values(this.val_Sub)[i]);
+                this.valuesKeys_Sub.push(Object.keys(this.val_Sub)[i], Object.values(this.val_Sub)[i]);
+
+                }
+             }
+          }
+    });
+  }
+
+  obtenerSubCategoriasInst(){
+    
+    
+    let body2 = new URLSearchParams();
+    body2.append('categoria', "Instrumentos");
+
+
+
+    this.http.post('http://emdpublicidad.com/tiendatalamas/archivos/php/obtenerSubCategoria.php', body2)
+    .map((res:Response) => res.json())
+            .subscribe(result => 
+              {
+              this.AA_Sub = "";
+            this.data_Sub = [];
+            console.log(result);
+            this.articulosArray_Inst = result;
+            for (var key in result) {
+            this.AA_Sub = this.AA_Sub + key;
+            if (result.hasOwnProperty(key)) {
+              this.val_Sub = result[key];
+              this.data_Sub.push(Object.keys(this.val_Sub));
+              for (var i = 0; i < Object.keys(this.val_Sub).length; i++) {
+              this.contenedor_Sub = Object.keys(this.val_Sub)[i];
+              Object.entries(this.val_Sub)[i]
+               
+                this.xxxMap_Sub.set(Object.keys(this.val_Sub)[i], Object.values(this.val_Sub)[i]);
+                this.valuesKeys_Sub.push(Object.keys(this.val_Sub)[i], Object.values(this.val_Sub)[i]);
+
+                }
+             }
+          }
+    });
+  }
+
+
+  navegarRegistro()
+  {
+    this.router.navigate(['registro']);
+    
+  }
+  navegarLibreria()
+  {
+    this.router.navigate(['libreria'])
+  }
+
+  navegarPublicidad()
+  {
+    this.router.navigate(['emd'])
+  }
+  navegarMusica()
+  {
+    this.router.navigate(['musica']);
+
+  }
+
+  navegarConfiguracion()
+  {
+    this.router.navigate(['ConfiguracionUsuario']);
+  }
+
+  navegarHistorial()
+  {
+    this.router.navigate(['historial']);
+  }
+  navegarBusqueda()
+  {
+    this.router.navigate(['busqueda',this.cadena])
+  }
+  navegarCarrito()
+  {
+    this.router.navigate(['Carrito'])
+  }
   navegarInicio()
   {
     this.router.navigate(['']);
