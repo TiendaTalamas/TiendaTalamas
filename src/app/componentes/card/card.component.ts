0f97f61cc57f3bd958c8cbe49a7e6524673968a0
Oscar MS <oscar-ms8@hotmail.com>
2020-09-21 15:47:04 -0500

    Registro funcionando

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -22,6 +22,12 @@ export class CardComponent implements OnInit {
   registro: FormGroup;
   registroForm: FormGroup;
   email: string;
+  emailR:string;
+  nombre:string;
+  apellido:string;
+  numero:string;
+  contrasena1:string;
+  contrasena2:string;
   contrasena: string;
   AA: string;
   data: any[];
@@ -42,7 +48,7 @@ ngOnInit(){
   {
     this.registroForm = fb.group({
       'email' : [null, Validators.required],
-  
+      'emailR' : [null, Validators.required],
       'contrasena': this.contrasena,
 
     });
@@ -85,6 +91,60 @@ navegarBusqueda()
                 }
 
     });
+
+    
+  }
+
+
+  registra() {     
+    if(this.nombre.length<3)
+    {
+      this.DatosError=true;
+      alert("La cadena debe ser mayor a 2 caracter")
+    }
+    if(this.nombre.includes("@"))
+    {
+      this.DatosError=true;
+      alert("Tiene un caracter no valido en el nombre")
+       
+    }
+    if(!this.DatosError)
+    {
+      
+    let body = new URLSearchParams();
+    body.append('nombre', this.nombre);
+    body.append('email', this.emailR);
+    body.append('apellido_pa', this.apellido);
+    body.append('apellido_ma', this.apellido);
+    body.append('numero_tel', this.numero);
+    body.append('contrasena', this.contrasena);
+
+    this.http.post(this._servicioCompartido.Url+'/registro.php', body)
+    .map((res:Response) => res.json())
+            .subscribe(result => 
+              {
+                console.log(result);
+                if(result['status'] == "400")
+                {
+                  this.respuesta=result['mensaje'];
+                  alert(this.respuesta);
+                }
+                else
+                {
+
+                  this.router.navigate(['Direccion']);
+                  this._servicioCompartido.Nombre = this.nombre;
+                  this._servicioCompartido.email = this.email;
+                  this._servicioCompartido.ApellidoPa = this.apellido;
+                  this._servicioCompartido.ApellidoMa = this.apellido
+                  this._servicioCompartido.NumeroTel = this.numero;
+                  this._servicioCompartido.contrasena = this.contrasena;
+                  this._servicioCompartido.soloRegistro = true;
+                }
+                
+          });
+
+    }
 
     
   }
