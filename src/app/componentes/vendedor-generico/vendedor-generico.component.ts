60d343e63676cfbae03cb0bb5f82f15c89197627
Oscar MS <oscar-ms8@hotmail.com>
2020-12-07 17:55:15 -0600

    Yuremama is my gf2

--- src/app/componentes/vendedor-generico/vendedor-generico.component.ts
+++ src/app/componentes/vendedor-generico/vendedor-generico.component.ts
@@ -60,7 +60,35 @@ export class VendedorGenericoComponent implements OnInit {
  
   ngOnInit()
   {
-
+    this.obtenerArticulosEspecificos();
+  }
+  
+  obtenerArticulosEspecificos() {
+    let body = new URLSearchParams();
+    body.append('marca','MELISSA REPOSTERIA');
+    this.http.post(this._servicioCompartido.Url+'/articulosPorMarca.php', body)
+    .map((res:Response) => res.json())
+            .subscribe(result => 
+            {
+            this.AA = "";
+            this.data = [];
+            this.articulosArray = result;
+            for (var key in result) {
+            this.AA = this.AA + key;
+            if (result.hasOwnProperty(key)) {
+              this.val = result[key];
+              this.data.push(Object.keys(this.val));
+              for (var i = 0; i < Object.keys(this.val).length; i++) {
+              this.contenedor = Object.keys(this.val)[i];
+              Object.entries(this.val)[i]
+               
+                this.xxxMap.set(Object.keys(this.val)[i], Object.values(this.val)[i]);
+                this.valuesKeys.push(Object.keys(this.val)[i], Object.values(this.val)[i]);
+                
+                }
+             }
+          }
+    });
   }
 
   masInformacion(IdProducto: string, Categoria: string, Nombre:string){
