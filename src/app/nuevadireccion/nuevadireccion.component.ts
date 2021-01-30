54386507a1adce8b64ebd44bd1b3ed6119d50f93
Oscar <oscar-ms8@hotmail.com>
2021-01-30 12:39:04 -0600

    Listo para subir

--- src/app/nuevadireccion/nuevadireccion.component.ts
+++ src/app/nuevadireccion/nuevadireccion.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class NuevadireccionComponent implements OnInit {
 
   constructor() { }
+  estado:string;
+  onChange(ok:string){
+
+  }
 
   ngOnInit() {
   }
