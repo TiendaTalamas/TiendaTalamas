e6c5d7a70b6e2f32b961af3436b7e11a4e307cae
Oscar Medallin <oscar-ms8@hotmail.com>
2021-05-14 16:24:34 -0500

    configuracion de usuario WORKING AT ALL

--- src/app/nuevadireccion/nuevadireccion.component.ts
+++ src/app/nuevadireccion/nuevadireccion.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import {Router} from "@angular/router"
 import { servicioCompartido } from 'src/app/servicios/servicioCompartido';
 import { Http, Response } from '@angular/http';
 import {Location} from "@angular/common"
+import { validateVerticalPosition } from '@angular/cdk/overlay';
 @Component({
   selector: 'app-nuevadireccion',
   templateUrl: './nuevadireccion.component.html',
@@ -12,15 +13,15 @@ import {Location} from "@angular/common"
 export class NuevadireccionComponent implements OnInit {
 
   formData:FormGroup
-  calle1:string;
-  calle2:string;
-  calle3:string;
-  colonia:string;
-  numExt:string;
-  numInterior:string;
-  codigoPost:string;
-  ciudad:string;
-  gustos:string;
+  calle1= new FormControl('',Validators.minLength(1));
+  calle2= new FormControl('',Validators.minLength(1));
+  calle3= new FormControl('',Validators.minLength(1));
+  colonia= new FormControl('',Validators.minLength(1));
+  numExt= new FormControl('',Validators.minLength(1));
+  numInterior= new FormControl('',Validators.minLength(1));
+  codigoPost = new FormControl('',Validators.minLength(1));
+  ciudad = new FormControl('',Validators.minLength(1));
+  estado = new FormControl('',Validators.minLength(1));
 
 
   constructor(private router: Router, private location:Location,private fb: FormBuilder,private http: Http, fb2: FormBuilder,public _servicioCompartido: servicioCompartido) {
@@ -34,20 +35,61 @@ export class NuevadireccionComponent implements OnInit {
       'codigoPost' : this.codigoPost,
       'estado' : this.estado,
       'ciudad': this.ciudad,
-      'gustos' : this.gustos
     });
-   }  estado:string;
+   }  
   nuevoLaredo:boolean;
+  nuevoLeon:boolean;
   onChange(Estado:string) {
-    if(Estado == "TAM" )
+    if(this.estado.value == "TAM" )
     {
       this.nuevoLaredo = true;
     }else{
       this.nuevoLaredo = false;
     }
+    if(this.estado.value == "NLE"){
+      this.nuevoLeon = true;
+    }else{
+      this.nuevoLeon = false;
+    }
+ }
+ cambiarDireccion()
+ {
+   let body = new URLSearchParams();
+ 
+   body.append('token', localStorage.getItem('Token'));
+   body.append('calle1', this.calle1.value);
+   body.append('calle2', this.calle2.value);
+   body.append('numero', this.numExt.value);
+   body.append('colonia', this.colonia.value);
+   body.append('codigoPostal', this.codigoPost.value);
+   body.append('ciudad', this.ciudad.value);
+   body.append('estado', this.estado.value);
+   body.append('pais', this.calle1.value);
+
+
+
+  
+   this.http.post(this._servicioCompartido.Url+'/agregarDireccion.php', body)
+   .map((res:Response) => res.text())
+           .subscribe(result => 
+             {
+               if(result == "OK")
+               {
+                 alert("Cambio realizado correctamente");
+               }
+               else if(result == "ERROR")
+               {
+                 alert("ERROR")
+               }
+   });
+
+   
  }
 
   ngOnInit() {
+    this.nuevoLaredo = false;
   }
+
+
 
 }
